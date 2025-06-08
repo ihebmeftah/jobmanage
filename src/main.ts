eecaed13fb5d8eb70aa5191d7b687c6a76131b9d
iheb meftah <ihebmeftah666@gmail.com>
2025-06-08 11:40:12 +0100

    feat(swagger): add Swagger integration for API documentation

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -10,6 +11,13 @@ async function bootstrap() {
       transform: true,
     }),
   );
+  const config = new DocumentBuilder()
+    .setTitle('Web Service project')
+    .setDescription('A project for managing job offers and candidate applications using **NestJS** and **GraphQL**.')
+    .setVersion('1.0')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
   await app.listen(parseInt(process.env.PORT!));
 }
 bootstrap();
